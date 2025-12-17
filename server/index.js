8e9afe938a3d1bd55564fe347747e70c048361a1
karanscriza <karan@scriza.co>
2025-12-17 11:53:37 +0530

    updated code

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
   })
 );
 app.use(cookieParser());
@@ -30,9 +30,10 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(PORT, () =>
-      console.log(`Server is listening on the port ${PORT}`)
-    );
+   app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
   } catch (error) {
     console.log(error);
   }
